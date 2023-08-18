02bfc30cb0b1c4fe698e69fa30ce692b6b64289c
Sj0-0i <sjy140801@dgu.ac.kr>
2023-08-18 23:28:59 +0900

    solved recursion

---
+++ 1629.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// Å« ¼öÀÇ °ÅµìÁ¦°ö
+// b°¡ Â¦¼öÀÌ¸é a^b = a^(b/2) * a^(b/2)
+// b°¡ È¦¼öÀÌ¸é a^b = a^(b/2) * a^(b/2 + 1)
+
+ll POW(ll a, ll b, ll c) {
+	if (b == 0)
+		return 1;
+	if (b == 1)
+		return a % c;
+
+	ll val = POW(a, b / 2, c);
+	val = val * val % c;
+
+	if (b % 2 == 0)  // Â¦¼öÀÏ ¶§
+		return val;
+
+	return val * a / c; // È¦¼öÀÏ ¶§
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+	cout << POW(a, b, c);
+	
+	return 0;
+}
